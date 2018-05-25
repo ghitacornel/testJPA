7ef23060c2d454df8dc606efa598e00f946897bc
cornel <ghita_cornel@yahoo.com>
2018-05-26 01:06:21 +0300

    better tests

--- src/test/java/entities/simple/TestEntityPersist.java
+++ src/test/java/entities/simple/TestEntityPersist.java
@@ -53,4 +53,55 @@ public class TestEntityPersist extends TransactionalSetup {
 
     }
 
+    @Test(expected = javax.persistence.PersistenceException.class)
+    public void testPersistTwice() {
+
+        // create new entity
+        Entity entity = new Entity();
+        entity.setId(1);
+        entity.setName("name");
+
+        // persist once + persistence context flushed and clear + persist twice = error
+        em.persist(entity);
+        flushAndClear();
+
+        em.persist(entity);
+        flushAndClear();
+
+    }
+
+    @Test
+    public void testPersistMultipleTimes() {
+
+        // create new entity
+        Entity entity1 = new Entity();
+        entity1.setId(1);
+        entity1.setName("name");
+
+        // persist multiple times but do not clear persistence context
+        em.persist(entity1);
+        em.persist(entity1);
+        em.flush();
+
+        em.persist(entity1);
+        flushAndClear();
+
+        // verify persist
+        Entity entity2 = em.find(Entity.class, entity1.getId());
+        Assert.assertNotNull(entity2);
+        ReflectionAssert.assertReflectionEquals(entity1, entity2);
+
+        // verify database state with a native query
+        {
+            List<Object[]> data = em.createNativeQuery("select id, name, nullableValue from SimpleEntity t").getResultList();
+            Assert.assertEquals(1, data.size());
+            for (Object[] objects : data) {
+                Assert.assertEquals(1, objects[0]);
+                Assert.assertEquals("name", objects[1]);
+                Assert.assertNull(objects[2]);
+            }
+        }
+
+    }
+
 }
