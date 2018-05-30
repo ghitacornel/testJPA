efe2bd9f1f38e5ef4a473c32bdd2c7fc7dabb080
cornel <ghita_cornel@yahoo.com>
2018-05-30 21:34:58 +0300

    better tests

--- src/test/java/relationships/one/to/one/unidirectional/child/parent/cascade/case2/TestCase2.java
+++ src/test/java/relationships/one/to/one/unidirectional/child/parent/cascade/case2/TestCase2.java
@@ -1,20 +1,20 @@
-package relationships.one.to.one.unidirectional.child.parent.cascade.case1;
+package relationships.one.to.one.unidirectional.child.parent.cascade.case2;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.unitils.reflectionassert.ReflectionAssert;
 import setup.TransactionalSetup;
 
-public class TestCase1 extends TransactionalSetup {
+public class TestCase2 extends TransactionalSetup {
 
     @Test
     public void test_InsertOnlyTheChild_AndObserve_TheParentIsAlsoInsertedDueToCascade() {
 
-        Case1Parent parent = new Case1Parent();
+        Case2Parent parent = new Case2Parent();
         parent.setId(1);
         parent.setName("parent");
 
-        Case1Child child = new Case1Child();
+        Case2Child child = new Case2Child();
         child.setId(1);
         child.setName("child");
         child.setParent(parent);
@@ -24,19 +24,19 @@ public class TestCase1 extends TransactionalSetup {
         flushAndClear();
 
         // verify child and parent are both inserted
-        ReflectionAssert.assertReflectionEquals(parent, em.find(Case1Parent.class, 1));
-        ReflectionAssert.assertReflectionEquals(child, em.find(Case1Child.class, 1));
+        ReflectionAssert.assertReflectionEquals(parent, em.find(Case2Parent.class, 1));
+        ReflectionAssert.assertReflectionEquals(child, em.find(Case2Child.class, 1));
 
     }
 
     @Test
     public void test_RemoveOnlyTheChild_AndObserve_TheParentIsAlsoRemovedDueToCascade() {
 
-        Case1Parent parent = new Case1Parent();
+        Case2Parent parent = new Case2Parent();
         parent.setId(1);
         parent.setName("parent");
 
-        Case1Child child = new Case1Child();
+        Case2Child child = new Case2Child();
         child.setId(1);
         child.setName("child");
         child.setParent(parent);
@@ -45,23 +45,23 @@ public class TestCase1 extends TransactionalSetup {
         flushAndClear();
 
         // remove only the child
-        em.remove(em.find(Case1Child.class, 1));
+        em.remove(em.find(Case2Child.class, 1));
         flushAndClear();
 
         // verify the parent is also removed
-        Assert.assertNull(em.find(Case1Parent.class, 1));
-        Assert.assertNull(em.find(Case1Child.class, 1));
+        Assert.assertNull(em.find(Case2Parent.class, 1));
+        Assert.assertNull(em.find(Case2Child.class, 1));
 
     }
 
     @Test(expected = javax.persistence.PersistenceException.class)
     public void test_RemoveOnlyTheParent_AndObserve_TheCascadeRemovalOfTheOrphanChildIsNotTriggeredHenceHenceExceptionIsRaisedDueToConstraint() {
 
-        Case1Parent parent = new Case1Parent();
+        Case2Parent parent = new Case2Parent();
         parent.setId(1);
         parent.setName("parent");
 
-        Case1Child child = new Case1Child();
+        Case2Child child = new Case2Child();
         child.setId(1);
         child.setName("child");
         child.setParent(parent);
@@ -69,21 +69,21 @@ public class TestCase1 extends TransactionalSetup {
         em.persist(child);
         flushAndClear();
 
-        em.remove(em.find(Case1Parent.class, 1));
+        em.remove(em.find(Case2Parent.class, 1));
         flushAndClear();
         // the parent is removed and not the child since the removed parent does not know about the related child
         // in this case JPA won't trigger a select to remove the orphaned child hence exception
 
     }
 
-    @Test(expected = javax.persistence.PersistenceException.class)
-    public void test_MarkTheParentOfTheChildAsNull_AndObserve_TheCascadeRemovalOfTheOrphanedChildIsNotTriggeredHenceExceptionIsRaisedDueToConstraint() {
+    @Test
+    public void test_TheMarkParentOfTheChildAsNull_AndObserve_TheParentIsRemovedDueToCascadeButTheOrphanedChildIsNotRemoved() {
 
-        Case1Parent parent = new Case1Parent();
+        Case2Parent parent = new Case2Parent();
         parent.setId(1);
         parent.setName("parent");
 
-        Case1Child child = new Case1Child();
+        Case2Child child = new Case2Child();
         child.setId(1);
         child.setName("child");
         child.setParent(parent);
@@ -91,9 +91,13 @@ public class TestCase1 extends TransactionalSetup {
         em.persist(child);
         flushAndClear();
 
-        // remove only the child
-        em.find(Case1Child.class, 1).setParent(null);
+        // mark the parent as null
+        em.find(Case2Child.class, 1).setParent(null);
         flushAndClear();
+
+        Assert.assertNull(em.find(Case2Parent.class, 1));
+        child.setParent(null);
+        ReflectionAssert.assertReflectionEquals(child, em.find(Case2Child.class, 1));
 
     }
 }
