2ca8ff95f2020a8f2b2063f27e781f8258355936
cornel <ghita_cornel@yahoo.com>
2018-06-01 14:55:55 +0300

    better tests

--- src/test/java/queries/named/nativ/TestNamedNativeQueryWithSqlResultSetMapping.java
+++ src/test/java/queries/named/nativ/TestNamedNativeQueryWithSqlResultSetMapping.java
@@ -20,13 +20,13 @@ public class TestNamedNativeQueryWithSqlResultSetMapping extends TransactionalSe
 
         Parent parent = new Parent();
         parent.setId(1);
-        parent.setName("parent 1");
+        parent.setName("parent " + parent.getId());
         parent.setChildren(new ArrayList<>());
 
         for (int i = 1; i <= 3; i++) {
             Child child = new Child();
             child.setId(i);
-            child.setName("child " + i);
+            child.setName("child " + +child.getId());
             child.setParent(parent);
             parent.getChildren().add(child);
         }
@@ -63,6 +63,54 @@ public class TestNamedNativeQueryWithSqlResultSetMapping extends TransactionalSe
 
         // verify fetched children
         ReflectionAssert.assertReflectionEquals(model.getChildren(), fetchedChildren, ReflectionComparatorMode.LENIENT_ORDER);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testFullyFetchedEntitiesAreManaged() {
+
+        // fetch
+        List<Object[]> existing = em.createNamedQuery("Parent.findWithChildrenNative").getResultList();
+
+        // alter
+        for (Object[] objects : existing) {
+            Parent parent = (Parent) objects[0];
+            parent.setName("new parent name " + parent.getId());
+            Child child = (Child) objects[1];
+            child.setName("new child name " + child.getId());
+        }
+
+        flushAndClear();
+
+        // fetch again
+        List<Object[]> updated = em.createNamedQuery("Parent.findWithChildrenNative").getResultList();
+
+        // adjust model for verification
+        {
+            model.setName("new parent name " + model.getId());
+            for (Child child : model.getChildren()) {
+                child.setName("new child name " + child.getId());
+            }
+
+        }
+        // verify, order is ignored
+        List<Child> fetchedChildren = new ArrayList<>();
+        for (Object[] objects : updated) {
+
+            // parent is 'retrieved' many times
+            Parent parent = (Parent) objects[0];
+            Child child = (Child) objects[1];
+            fetchedChildren.add(child);
+
+            Assert.assertEquals(model.getId(), parent.getId());
+            Assert.assertEquals(model.getName(), parent.getName());
+
+        }
+
+        // verify fetched children
+        ReflectionAssert.assertReflectionEquals(model.getChildren(), fetchedChildren, ReflectionComparatorMode.LENIENT_ORDER);
+
 
     }
 
