7711dd4c9e65f3758ee471f4ef8d5e61754ee902
cornel <ghita_cornel@yahoo.com>
2018-06-01 12:27:41 +0300

    better tests

--- src/test/java/queries/simple/TestSimpleQueries.java
+++ src/test/java/queries/simple/TestSimpleQueries.java
@@ -60,13 +60,20 @@ public class TestSimpleQueries extends TransactionalSetup {
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(7);
+            entity.setName("jOHn");
+            entity.setValue(0);
+            list.add(entity);
+        }
+        {
+            SimpleQueryEntity entity = new SimpleQueryEntity();
+            entity.setId(8);
             entity.setName("a john 1");
             entity.setValue(1);
             list.add(entity);
         }
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(8);
+            entity.setId(9);
             entity.setName("a second JOHN 2");
             entity.setValue(2);
             list.add(entity);
@@ -211,8 +218,8 @@ public class TestSimpleQueries extends TransactionalSetup {
     @Test
     public void testWithLikeClauseBAD() {
 
-        List<SimpleQueryEntity> list = em.createQuery("select e from SQE e where e.name like :name", SimpleQueryEntity.class).setParameter("name", "john").getResultList();
-        ReflectionAssert.assertReflectionEquals(new ArrayList<>(), list);
+        List<SimpleQueryEntity> list = em.createQuery("select e from SQE e where e.name like :name", SimpleQueryEntity.class).setParameter("name", "jOHn").getResultList();
+        ReflectionAssert.assertReflectionEquals(buildModel().get(6), list.get(0));
 
     }
 
@@ -223,15 +230,19 @@ public class TestSimpleQueries extends TransactionalSetup {
     public void testWithLikeClauseStillBAD() {
 
         List<SimpleQueryEntity> list = em.createQuery("select e from SQE e where e.name like :name", SimpleQueryEntity.class).setParameter("name", "%john%").getResultList();
-        ReflectionAssert.assertReflectionEquals(buildModel().get(6), list.get(0));
+        ReflectionAssert.assertReflectionEquals(buildModel().get(7), list.get(0));
 
     }
 
+    /**
+     * note the usage of "lower" on both sides
+     * note the usage of %% for position match
+     */
     @Test
     public void testWithLikeClauseOK() {
 
         List<SimpleQueryEntity> list = em.createQuery("select e from SQE e where lower(e.name) like lower(:name)", SimpleQueryEntity.class).setParameter("name", "%john%").getResultList();
-        ReflectionAssert.assertReflectionEquals(buildModel().subList(6, 8), list);
+        ReflectionAssert.assertReflectionEquals(buildModel().subList(6, 9), list);
 
     }
 }
