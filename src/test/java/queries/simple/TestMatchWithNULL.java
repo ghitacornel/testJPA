0fd51d8775995a59b0eaca83079b087501f01ef0
cornel <ghita_cornel@yahoo.com>
2018-06-05 02:28:05 +0300

    better tests

--- src/test/java/queries/simple/TestMatchWithNULL.java
+++ src/test/java/queries/simple/TestMatchWithNULL.java
@@ -17,63 +17,14 @@ public class TestMatchWithNULL extends TransactionalSetup {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(1);
             entity.setName("name 1");
-            entity.setValue(6);
+            entity.setValue(1);
             list.add(entity);
         }
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(2);
             entity.setName("name 2");
-            entity.setValue(5);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(3);
-            entity.setName("name 3");
-            entity.setValue(4);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(4);
-            entity.setName("name 4");
-            entity.setValue(3);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(5);
-            entity.setName("name 5");
-            entity.setValue(2);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(6);
-            entity.setName("name 6");
             entity.setValue(null);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(7);
-            entity.setName("jOHn");
-            entity.setValue(0);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(8);
-            entity.setName("a john 1");
-            entity.setValue(1);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(9);
-            entity.setName("a second JOHN 2");
-            entity.setValue(2);
             list.add(entity);
         }
         return list;
@@ -92,7 +43,7 @@ public class TestMatchWithNULL extends TransactionalSetup {
         // CORRECT way to check for NULL
         List<SimpleQueryEntity> list = em.createQuery("select e from SQE e where e.value IS NULL", SimpleQueryEntity.class).getResultList();
         List<SimpleQueryEntity> expected = new ArrayList<>();
-        expected.add(buildModel().get(5));
+        expected.add(buildModel().get(1));
         ReflectionAssert.assertReflectionEquals(expected, list);
 
     }
