0a6ebff61ba0afd14473bf62ffa032bc87da3166
cornel <ghita_cornel@yahoo.com>
2018-06-05 02:37:31 +0300

    better tests

--- src/test/java/queries/simple/TestCOUNT.java
+++ src/test/java/queries/simple/TestCOUNT.java
@@ -16,63 +16,21 @@ public class TestCOUNT extends TransactionalSetup {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(1);
             entity.setName("name 1");
-            entity.setValue(6);
+            entity.setValue(1);
             list.add(entity);
         }
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(2);
             entity.setName("name 2");
-            entity.setValue(5);
+            entity.setValue(2);
             list.add(entity);
         }
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(3);
             entity.setName("name 3");
-            entity.setValue(4);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(4);
-            entity.setName("name 4");
             entity.setValue(3);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(5);
-            entity.setName("name 5");
-            entity.setValue(2);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(6);
-            entity.setName("name 6");
-            entity.setValue(null);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(7);
-            entity.setName("jOHn");
-            entity.setValue(0);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(8);
-            entity.setName("a john 1");
-            entity.setValue(1);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(9);
-            entity.setName("a second JOHN 2");
-            entity.setValue(2);
             list.add(entity);
         }
         return list;
@@ -84,20 +42,15 @@ public class TestCOUNT extends TransactionalSetup {
         flushAndClear();
     }
 
-
     @Test
     public void testCountAll() {
-
         Long count = em.createQuery("select count(e) from SQE e", Long.class).getSingleResult();
         Assert.assertEquals(buildModel().size(), count.longValue());
-
     }
 
     @Test
     public void testCountSome() {
-
-        Long count = em.createQuery("select count(e) from SQE e where e.value = 1 or e.value = 2 or e.value = 3", Long.class).getSingleResult();
-        Assert.assertEquals(4, count.longValue());
-
+        Long count = em.createQuery("select count(e) from SQE e where e.value = 2 or e.value = 3", Long.class).getSingleResult();
+        Assert.assertEquals(2, count.longValue());
     }
 }
