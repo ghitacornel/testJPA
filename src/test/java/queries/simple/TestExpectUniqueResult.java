693835f4525c21c4798c7725e6c218cac160f4e9
cornel <ghita_cornel@yahoo.com>
2018-06-05 02:36:05 +0300

    better tests

--- src/test/java/queries/simple/TestExpectUniqueResult.java
+++ src/test/java/queries/simple/TestExpectUniqueResult.java
@@ -16,63 +16,21 @@ public class TestExpectUniqueResult extends TransactionalSetup {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(1);
             entity.setName("name 1");
-            entity.setValue(6);
+            entity.setValue(1);
             list.add(entity);
         }
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(2);
             entity.setName("name 2");
-            entity.setValue(5);
+            entity.setValue(2);
             list.add(entity);
         }
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(3);
             entity.setName("name 3");
-            entity.setValue(4);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(4);
-            entity.setName("name 4");
             entity.setValue(3);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(5);
-            entity.setName("name 5");
-            entity.setValue(2);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(6);
-            entity.setName("name 6");
-            entity.setValue(null);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(7);
-            entity.setName("jOHn");
-            entity.setValue(0);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(8);
-            entity.setName("a john 1");
-            entity.setValue(1);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(9);
-            entity.setName("a second JOHN 2");
-            entity.setValue(2);
             list.add(entity);
         }
         return list;
@@ -86,20 +44,18 @@ public class TestExpectUniqueResult extends TransactionalSetup {
 
     @Test
     public void testUniqueResult() {
-
-        SimpleQueryEntity entity = em.createQuery("select e from SQE e where e.name = 'name 1'", SimpleQueryEntity.class).getSingleResult();
+        SimpleQueryEntity entity = em.createQuery("select e from SQE e where e.value = 1", SimpleQueryEntity.class).getSingleResult();
         ReflectionAssert.assertReflectionEquals(buildModel().get(0), entity);
-
     }
 
     @Test(expected = javax.persistence.NonUniqueResultException.class)
     public void testUniqueResultNonUniqueSelect() {
-        em.createQuery("select e from SQE e where e.name = 'name 1' or e.name = 'name 2'", SimpleQueryEntity.class).getSingleResult();
+        em.createQuery("select e from SQE e where e.value = 2 or e.value = 3", SimpleQueryEntity.class).getSingleResult();
     }
 
     @Test(expected = javax.persistence.NoResultException.class)
     public void testUniqueResultNoData() {
-        em.createQuery("select e from SQE e where e.name = 'name not present'", SimpleQueryEntity.class).getSingleResult();
+        em.createQuery("select e from SQE e where e.value = 4", SimpleQueryEntity.class).getSingleResult();
     }
 
 }
