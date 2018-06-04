8be041ad9b031a88a9c7d0ec5ff69a7804246534
cornel <ghita_cornel@yahoo.com>
2018-06-05 02:41:55 +0300

    better tests

--- src/test/java/queries/simple/TestFindByUsingLIKE.java
+++ src/test/java/queries/simple/TestFindByUsingLIKE.java
@@ -28,34 +28,6 @@ public class TestFindByUsingLIKE extends TransactionalSetup {
         }
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(3);
-            entity.setName("name 3");
-            entity.setValue(4);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(4);
-            entity.setName("name 4");
-            entity.setValue(3);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(5);
-            entity.setName("name 5");
-            entity.setValue(2);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(6);
-            entity.setName("name 6");
-            entity.setValue(null);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(7);
             entity.setName("jOHn");
             entity.setValue(0);
@@ -91,7 +63,7 @@ public class TestFindByUsingLIKE extends TransactionalSetup {
     public void testWithLikeClauseBAD() {
 
         List<SimpleQueryEntity> list = em.createQuery("select e from SQE e where e.name like :name", SimpleQueryEntity.class).setParameter("name", "jOHn").getResultList();
-        ReflectionAssert.assertReflectionEquals(buildModel().get(6), list.get(0));
+        ReflectionAssert.assertReflectionEquals(buildModel().get(2), list.get(0));
 
     }
 
@@ -102,7 +74,7 @@ public class TestFindByUsingLIKE extends TransactionalSetup {
     public void testWithLikeClauseStillBAD() {
 
         List<SimpleQueryEntity> list = em.createQuery("select e from SQE e where e.name like :name", SimpleQueryEntity.class).setParameter("name", "%john%").getResultList();
-        ReflectionAssert.assertReflectionEquals(buildModel().get(7), list.get(0));
+        ReflectionAssert.assertReflectionEquals(buildModel().get(3), list.get(0));
 
     }
 
@@ -114,7 +86,7 @@ public class TestFindByUsingLIKE extends TransactionalSetup {
     public void testWithLikeClauseOK() {
 
         List<SimpleQueryEntity> list = em.createQuery("select e from SQE e where lower(e.name) like lower(:name)", SimpleQueryEntity.class).setParameter("name", "%john%").getResultList();
-        ReflectionAssert.assertReflectionEquals(buildModel().subList(6, 9), list);
+        ReflectionAssert.assertReflectionEquals(buildModel().subList(2, 5), list);
 
     }
 }
