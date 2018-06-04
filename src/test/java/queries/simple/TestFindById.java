ef7030318308cb739c205c653a3029b72fc12920
cornel <ghita_cornel@yahoo.com>
2018-06-05 02:34:27 +0300

    better tests

--- src/test/java/queries/simple/TestFindById.java
+++ src/test/java/queries/simple/TestFindById.java
@@ -1,5 +1,6 @@
 package queries.simple;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.unitils.reflectionassert.ReflectionAssert;
@@ -16,62 +17,13 @@ public class TestFindById extends TransactionalSetup {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(1);
             entity.setName("name 1");
-            entity.setValue(6);
+            entity.setValue(1);
             list.add(entity);
         }
         {
             SimpleQueryEntity entity = new SimpleQueryEntity();
             entity.setId(2);
             entity.setName("name 2");
-            entity.setValue(5);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(3);
-            entity.setName("name 3");
-            entity.setValue(4);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(4);
-            entity.setName("name 4");
-            entity.setValue(3);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(5);
-            entity.setName("name 5");
-            entity.setValue(2);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(6);
-            entity.setName("name 6");
-            entity.setValue(null);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(7);
-            entity.setName("jOHn");
-            entity.setValue(0);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(8);
-            entity.setName("a john 1");
-            entity.setValue(1);
-            list.add(entity);
-        }
-        {
-            SimpleQueryEntity entity = new SimpleQueryEntity();
-            entity.setId(9);
-            entity.setName("a second JOHN 2");
             entity.setValue(2);
             list.add(entity);
         }
@@ -85,10 +37,18 @@ public class TestFindById extends TransactionalSetup {
     }
 
     @Test
-    public void testFindByIdNoQuery() {
+    public void testFindByIdExistingId() {
 
-        SimpleQueryEntity entity = em.find(SimpleQueryEntity.class, 5);
-        ReflectionAssert.assertReflectionEquals(buildModel().get(4), entity);
+        SimpleQueryEntity entity = em.find(SimpleQueryEntity.class, 1);
+        ReflectionAssert.assertReflectionEquals(buildModel().get(0), entity);
+
+    }
+
+    @Test
+    public void testFindByIdNotExistingId() {
+
+        SimpleQueryEntity entity = em.find(SimpleQueryEntity.class, -1);
+        Assert.assertNull(entity);
 
     }
 
