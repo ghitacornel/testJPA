7c22ba4804f403ec6e2b2d16937f9ce8a7c2489a
cornel <ghita_cornel@yahoo.com>
2018-06-05 00:53:03 +0300

    better tests

--- src/test/java/relationships/manytoone/notstrict/TestCRUD.java
+++ src/test/java/relationships/manytoone/notstrict/TestCRUD.java
@@ -16,6 +16,8 @@ public class TestCRUD extends TransactionalSetup {
     @Before
     public void before() {
 
+        verifyCorrespondingTableIsEmpty(MTOONotStrictChild.class);
+
         parent1.setId(1);
         parent1.setName("parent strict 1");
         persist(parent1);
@@ -29,10 +31,6 @@ public class TestCRUD extends TransactionalSetup {
 
     @Test
     public void testCRUD() {
-
-        // assert empty
-        Assert.assertTrue(em.createQuery("select t from MTOONotStrictChild t").getResultList().isEmpty());
-        flushAndClear();
 
         // insert
         MTOONotStrictChild child = new MTOONotStrictChild();
@@ -49,63 +47,26 @@ public class TestCRUD extends TransactionalSetup {
         flushAndClear();
 
         // update
-        MTOONotStrictChild existing = em.find(MTOONotStrictChild.class, 1);
+        MTOONotStrictChild existing = em.find(MTOONotStrictChild.class, child.getId());
         existing.setName("new child name");// update name
         existing.setParent(parent2);// update parent
         em.merge(existing);
         flushAndClear();
 
         // test update
-        existing = em.find(MTOONotStrictChild.class, 1);
+        existing = em.find(MTOONotStrictChild.class, child.getId());
         Assert.assertEquals("new child name", existing.getName());
         ReflectionAssert.assertReflectionEquals(parent2, existing.getParent());
         flushAndClear();
 
-        // update and set parent to null
-        existing = em.find(MTOONotStrictChild.class, 1);
-        existing.setParent(null);
-        flushAndClear();
-
-        // test set parent to null
-        existing = em.find(MTOONotStrictChild.class, 1);
-        Assert.assertNull(existing.getParent());
-        flushAndClear();
-
         // remove
-        existing = em.find(MTOONotStrictChild.class, 1);
+        existing = em.find(MTOONotStrictChild.class, child.getId());
         Assert.assertNotNull(existing);
         em.remove(existing);
         flushAndClear();
 
         // test remove
         Assert.assertTrue(em.createQuery("select t from MTOONotStrictChild t").getResultList().isEmpty());
-
-    }
-
-    @Test
-    public void testSetParentToNull() {
-
-        // assert empty
-        Assert.assertTrue(em.createQuery("select t from MTOONotStrictChild t").getResultList().isEmpty());
-        flushAndClear();
-
-        // insert
-        MTOONotStrictChild child = new MTOONotStrictChild();
-        child.setId(1);
-        child.setName("child name");
-        child.setParent(parent1);
-        em.persist(child);
-        flushAndClear();
-
-        // update and set parent to null
-        MTOONotStrictChild existing = em.find(MTOONotStrictChild.class, 1);
-        existing.setParent(null);
-        flushAndClear();
-
-        // test set parent to null
-        existing = em.find(MTOONotStrictChild.class, 1);
-        Assert.assertNull(existing.getParent());
-        flushAndClear();
 
     }
 
---
+++ src/test/java/relationships/manytoone/notstrict/TestSetParentToNull.java
@@ -0,0 +1,48 @@
+package relationships.manytoone.notstrict;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.unitils.reflectionassert.ReflectionAssert;
+import setup.TransactionalSetup;
+
+public class TestSetParentToNull extends TransactionalSetup {
+
+    private MTOONotStrictParent parent = new MTOONotStrictParent();
+
+    @Before
+    public void before() {
+
+        verifyCorrespondingTableIsEmpty(MTOONotStrictChild.class);
+
+        parent.setId(1);
+        parent.setName("parent name");
+        persist(parent);
+
+        flushAndClear();
+    }
+
+    @Test
+    public void testSetParentToNull() {
+
+        // insert
+        MTOONotStrictChild child = new MTOONotStrictChild();
+        child.setId(1);
+        child.setName("child name");
+        child.setParent(parent);
+        em.persist(child);
+        flushAndClear();
+
+        // update and set parent to null
+        MTOONotStrictChild existing = em.find(MTOONotStrictChild.class, child.getId());
+        existing.setParent(null);
+        flushAndClear();
+
+        // test set parent to null
+        existing = em.find(MTOONotStrictChild.class, child.getId());
+        Assert.assertNull(existing.getParent());
+        ReflectionAssert.assertReflectionEquals(parent, em.find(MTOONotStrictParent.class, parent.getId()));
+
+    }
+
+}
