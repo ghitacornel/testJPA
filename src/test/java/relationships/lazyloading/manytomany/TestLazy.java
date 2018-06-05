c49a5d851aab41bf0128022c3826731cd323f6ae
cornel <ghita_cornel@yahoo.com>
2018-06-05 08:20:57 +0300

    better tests

---
+++ src/test/java/relationships/lazyloading/manytomany/TestLazy.java
@@ -0,0 +1,60 @@
+package relationships.lazyloading.manytomany;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import setup.TransactionalSetup;
+
+import javax.persistence.Persistence;
+
+public class TestLazy extends TransactionalSetup {
+
+    MTOMLazyM m;
+    MTOMLazyN n;
+
+    @Before
+    public void setUp() {
+
+        m = new MTOMLazyM();
+        m.setId(1);
+        m.setName("m");
+
+        n = new MTOMLazyN();
+        n.setId(1);
+        n.setName("n");
+
+        m.getListWithNs().add(n);
+        n.getListWithMs().add(m);
+
+        persist(m, n);
+        flushAndClear();
+
+    }
+
+    @Test
+    public void testLazyLoaded() {
+
+        MTOMLazyM existingM = em.find(MTOMLazyM.class, m.getId());
+        MTOMLazyN existingN = em.find(MTOMLazyN.class, n.getId());
+        flushAndClear();
+
+        Assert.assertFalse(Persistence.getPersistenceUtil().isLoaded(existingM.getListWithNs()));
+        Assert.assertFalse(Persistence.getPersistenceUtil().isLoaded(existingN.getListWithMs()));
+
+    }
+
+    @Test
+    public void testForceEager() {
+
+        MTOMLazyM existingM = em.find(MTOMLazyM.class, m.getId());
+        existingM.getListWithNs().size();
+        MTOMLazyN existingN = em.find(MTOMLazyN.class, n.getId());
+        existingN.getListWithMs().size();
+        flushAndClear();
+
+        Assert.assertEquals(1, existingM.getListWithNs().size());
+        Assert.assertEquals(1, existingN.getListWithMs().size());
+
+    }
+
+}
