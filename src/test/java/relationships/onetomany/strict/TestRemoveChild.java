f4b68b034059dcf9365a93439c62545814db6221
cornel <ghita_cornel@yahoo.com>
2018-06-11 08:56:25 +0300

    better tests

--- src/test/java/relationships/onetomany/strict/TestRemoveChild.java
+++ src/test/java/relationships/onetomany/strict/TestRemoveChild.java
@@ -48,7 +48,7 @@ public class TestRemoveChild extends TransactionalSetup {
         ReflectionAssert.assertReflectionEquals(expectedParent, existingParent, ReflectionComparatorMode.LENIENT_ORDER);
 
         // child is removed since it is now an orphan
-        OTOMStrictChild child = em.find(OTOMStrictChild.class, 1);
+        OTOMStrictChild child = em.find(OTOMStrictChild.class, buildModel().getChildren().get(1).getId());
         Assert.assertNull(child);
 
     }
@@ -58,6 +58,68 @@ public class TestRemoveChild extends TransactionalSetup {
 
         em.find(OTOMStrictParent.class, model.getId()).getChildren().get(1).setParent(null);
         flushAndClear();
+
+    }
+
+    @Test
+    public void testRemoveChild() {
+
+        em.remove(em.find(OTOMStrictChild.class, 1));
+        flushAndClear();
+
+        // test parent and other children are unaffected
+        OTOMStrictParent expectedParent = buildModel();
+        {// adjust model to reflect expected changes
+            expectedParent.getChildren().remove(0);
+        }
+        ReflectionAssert.assertReflectionEquals(expectedParent, em.find(OTOMStrictParent.class, expectedParent.getId()), ReflectionComparatorMode.LENIENT_ORDER);
+
+        // test child is now removed
+        Assert.assertNull(em.find(OTOMStrictChild.class, 1));
+
+    }
+
+    @Test
+    public void testRemoveChildWhenParentAndSiblingsAreLoadedDoesNotTriggerTheRemoval() {
+
+        OTOMStrictParent existingParent = em.find(OTOMStrictParent.class, 1);
+        Assert.assertEquals(3, existingParent.getChildren().size());// verify number of children
+
+        OTOMStrictChild toRemoveChild = em.find(OTOMStrictChild.class, 1);
+        em.remove(toRemoveChild);
+        Assert.assertEquals(3, existingParent.getChildren().size());// verify number of children is the same
+        flushAndClear();
+
+        // test parent and other children are unaffected
+        OTOMStrictParent expectedParent = buildModel();
+        ReflectionAssert.assertReflectionEquals(expectedParent, em.find(OTOMStrictParent.class, expectedParent.getId()), ReflectionComparatorMode.LENIENT_ORDER);
+
+        // test child is now removed
+        Assert.assertNotNull(em.find(OTOMStrictChild.class, toRemoveChild.getId()));
+
+    }
+
+    @Test
+    public void testRemoveChildWhenParentAndSiblingsAreLoadedWorks() {
+
+        OTOMStrictParent existingParent = em.find(OTOMStrictParent.class, 1);
+        Assert.assertEquals(3, existingParent.getChildren().size());// verify number of children
+
+        OTOMStrictChild toRemoveChild = em.find(OTOMStrictChild.class, 1);
+        existingParent.getChildren().remove(toRemoveChild);
+        em.remove(toRemoveChild);
+        Assert.assertEquals(2, existingParent.getChildren().size());// verify number of children
+        flushAndClear();
+
+        // test parent and other children are unaffected
+        OTOMStrictParent expectedParent = buildModel();
+        {// adjust model to reflect expected changes
+            expectedParent.getChildren().remove(0);
+        }
+        ReflectionAssert.assertReflectionEquals(expectedParent, em.find(OTOMStrictParent.class, expectedParent.getId()), ReflectionComparatorMode.LENIENT_ORDER);
+
+        // test child is now removed
+        Assert.assertNull(em.find(OTOMStrictChild.class, toRemoveChild.getId()));
 
     }
 }
