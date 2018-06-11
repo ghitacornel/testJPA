06e2e0b8439773fd671e6af2f00b4c0511872fd8
cornel <ghita_cornel@yahoo.com>
2018-06-11 20:32:26 +0300

    better tests

---
+++ src/test/java/relationships/orderby/manytomany/TestSelectAll.java
@@ -0,0 +1,41 @@
+package relationships.orderby.manytomany;
+
+import org.junit.Test;
+import org.unitils.reflectionassert.ReflectionAssert;
+import setup.TransactionalSetup;
+
+import java.util.Collections;
+
+public class TestSelectAll extends TransactionalSetup {
+
+    @Test
+    public void testSelectAllInOrder() {
+
+        // create model
+        MTOMOrderM m = new MTOMOrderM();
+        m.setId(1);
+        m.setName("m");
+
+        MTOMOrderN n1 = new MTOMOrderN();
+        n1.setId(1);
+        n1.setName("n1");
+
+        MTOMOrderN n2 = new MTOMOrderN();
+        n2.setId(2);
+        n2.setName("n2");
+
+        m.getListWithNs().add(n2);
+        m.getListWithNs().add(n1);
+
+        persist(m);
+        flushAndClear();
+
+        // verify order
+        {// adjust model to expected result
+            Collections.reverse(m.getListWithNs());
+        }
+
+        ReflectionAssert.assertReflectionEquals(m, em.find(MTOMOrderM.class, m.getId()));
+
+    }
+}
