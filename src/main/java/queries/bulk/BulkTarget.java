804a2214d3e0fb32164fd50d4e5f3698ab29b9fa
cornel <ghita_cornel@yahoo.com>
2018-06-12 23:54:34 +0300

    better tests

---
+++ src/main/java/queries/bulk/BulkTarget.java
@@ -0,0 +1,34 @@
+package queries.bulk;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class BulkTarget {
+
+    @Id
+    private Integer id;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
