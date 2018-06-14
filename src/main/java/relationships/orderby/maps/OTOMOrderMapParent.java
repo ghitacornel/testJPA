ab3f7391372755b322c4f17039bbf55d8fc5986a
cornel <ghita_cornel@yahoo.com>
2018-06-14 23:48:44 +0300

    better tests

---
+++ src/main/java/relationships/orderby/maps/OTOMOrderMapParent.java
@@ -0,0 +1,46 @@
+package relationships.orderby.maps;
+
+import javax.persistence.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Entity
+public class OTOMOrderMapParent {
+
+    @Id
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @MapKey(name = "id")
+    @OrderBy("name")
+    final private Map<Integer, OTOMOrderMapChild> children = new LinkedHashMap<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<Integer, OTOMOrderMapChild> getChildren() {
+        return children;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getClass() + "|" + id + "|" + name + "]";
+    }
+
+}
