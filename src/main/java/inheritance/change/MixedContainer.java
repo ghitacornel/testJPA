769e9804b2544332662b09268ba4638cbfc838f4
cornel <ghita_cornel@yahoo.com>
2018-06-15 18:11:17 +0300

    better tests

--- src/main/java/inheritance/change/MixedContainer.java
+++ src/main/java/inheritance/change/MixedContainer.java
@@ -17,15 +17,15 @@ public class MixedContainer extends InheritanceMappedSuperClass {
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "reference_id")
-    private List<ConcreteSuperClass> concreteSuperClass = new ArrayList<>();
+    final private List<ConcreteSuperClass> concreteSuperClass = new ArrayList<>();
 
     @OneToMany(cascade = CascadeType.ALL, targetEntity = ConcreteClassA.class)
     @JoinColumn(name = "reference_id_a")
-    private List<ConcreteClassA> concreteClassAs = new ArrayList<>();
+    final private List<ConcreteClassA> concreteClassAs = new ArrayList<>();
 
     @OneToMany(cascade = CascadeType.ALL, targetEntity = ConcreteClassB.class)
     @JoinColumn(name = "reference_id_b")
-    private List<ConcreteClassB> concreteClassBs = new ArrayList<>();
+    final private List<ConcreteClassB> concreteClassBs = new ArrayList<>();
 
     public String getName() {
         return name;
@@ -39,23 +39,12 @@ public class MixedContainer extends InheritanceMappedSuperClass {
         return concreteClassBs;
     }
 
-    public void setConcreteClassBs(List<ConcreteClassB> concreteClassBs) {
-        this.concreteClassBs = concreteClassBs;
-    }
-
     public List<ConcreteSuperClass> getConcreteSuperClass() {
         return concreteSuperClass;
-    }
-
-    public void setConcreteSuperClass(List<ConcreteSuperClass> concreteSuperClass) {
-        this.concreteSuperClass = concreteSuperClass;
     }
 
     public List<ConcreteClassA> getConcreteClassAs() {
         return concreteClassAs;
     }
 
-    public void setConcreteClassAs(List<ConcreteClassA> concreteClassAs) {
-        this.concreteClassAs = concreteClassAs;
-    }
 }
