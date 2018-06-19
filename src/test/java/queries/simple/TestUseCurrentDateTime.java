f6ce0a196a644561b6959f1aaa04d60810c50e23
cornel <ghita_cornel@yahoo.com>
2018-06-19 14:46:13 +0300

    better tests

--- src/test/java/queries/simple/TestUseCurrentDateTime.java
+++ src/test/java/queries/simple/TestUseCurrentDateTime.java
@@ -13,9 +13,12 @@ import java.util.List;
 
 public class TestUseCurrentDateTime extends TransactionalSetup {
 
+    Date tomorrow;
+
     EntityWithDate entityNow;
     EntityWithDate entityYesterday;
     EntityWithDate entityTomorrow;
+    EntityWithDate entityTomorrowMinusOneHour;
 
     @Before
     public void before() {
@@ -28,18 +31,21 @@ public class TestUseCurrentDateTime extends TransactionalSetup {
             Calendar instance = Calendar.getInstance();
             instance.setTime(now);
             instance.add(Calendar.DAY_OF_YEAR, -1);
-            instance.add(Calendar.HOUR, -1);
-            instance.add(Calendar.MINUTE, -1);
             yesterday = instance.getTime();
         }
-        Date tomorrow;
         {
             Calendar instance = Calendar.getInstance();
             instance.setTime(now);
             instance.add(Calendar.DAY_OF_YEAR, 1);
-            instance.add(Calendar.HOUR, 1);
-            instance.add(Calendar.MINUTE, 1);
             tomorrow = instance.getTime();
+        }
+        Date tomorrowMinusOneHour;
+        {
+            Calendar instance = Calendar.getInstance();
+            instance.setTime(now);
+            instance.add(Calendar.DAY_OF_YEAR, 1);
+            instance.add(Calendar.HOUR, -1);
+            tomorrowMinusOneHour = instance.getTime();
         }
 
         entityYesterday = new EntityWithDate();
@@ -63,6 +69,13 @@ public class TestUseCurrentDateTime extends TransactionalSetup {
         entityTomorrow.setOnlyDate(tomorrow);
         persist(entityTomorrow);
 
+        entityTomorrowMinusOneHour = new EntityWithDate();
+        entityTomorrowMinusOneHour.setId(4);
+        entityTomorrowMinusOneHour.setFullDate(tomorrowMinusOneHour);
+        entityTomorrowMinusOneHour.setOnlyTime(tomorrowMinusOneHour);
+        entityTomorrowMinusOneHour.setOnlyDate(tomorrowMinusOneHour);
+        persist(entityTomorrowMinusOneHour);
+
         flushAndClear();
     }
 
@@ -77,17 +90,30 @@ public class TestUseCurrentDateTime extends TransactionalSetup {
     @Test
     public void testGetCurrentAndFuture() {
 
-        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.onlyDate >= current_date order by id", EntityWithDate.class).getResultList();
-        Assert.assertEquals(2, existing.size());
+        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.onlyDate >= current_date order by fullDate", EntityWithDate.class).getResultList();
+        Assert.assertEquals(3, existing.size());
         verifyEquals(entityNow, existing.get(0));
-        verifyEquals(entityTomorrow, existing.get(1));
+        verifyEquals(entityTomorrowMinusOneHour, existing.get(1));
+        verifyEquals(entityTomorrow, existing.get(2));
 
     }
 
     @Test
     public void testGetOnlyFuture() {
 
-        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.onlyDate > current_date order by id", EntityWithDate.class).getResultList();
+        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.onlyDate > current_date order by fullDate", EntityWithDate.class).getResultList();
+        Assert.assertEquals(2, existing.size());
+        verifyEquals(entityTomorrowMinusOneHour, existing.get(0));
+        verifyEquals(entityTomorrow, existing.get(1));
+
+    }
+
+    @Test
+    public void testGetOnlyFutureFromTomorrow() {
+
+        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.fullDate >= ?1 order by fullDate", EntityWithDate.class).
+                setParameter(1, tomorrow).
+                getResultList();
         Assert.assertEquals(1, existing.size());
         verifyEquals(entityTomorrow, existing.get(0));
 
@@ -96,7 +122,7 @@ public class TestUseCurrentDateTime extends TransactionalSetup {
     @Test
     public void testGetCurrentAndPast() {
 
-        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.onlyDate <= current_date order by id", EntityWithDate.class).getResultList();
+        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.onlyDate <= current_date order by fullDate", EntityWithDate.class).getResultList();
         Assert.assertEquals(2, existing.size());
         verifyEquals(entityYesterday, existing.get(0));
         verifyEquals(entityNow, existing.get(1));
@@ -106,7 +132,7 @@ public class TestUseCurrentDateTime extends TransactionalSetup {
     @Test
     public void testGetOnlyPast() {
 
-        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.onlyDate < current_date order by id", EntityWithDate.class).getResultList();
+        List<EntityWithDate> existing = em.createQuery("select e from EntityWithDate e where e.onlyDate < current_date order by fullDate", EntityWithDate.class).getResultList();
         Assert.assertEquals(1, existing.size());
         verifyEquals(entityYesterday, existing.get(0));
 
