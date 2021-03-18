8a30731ac5176a93db2ca7b27d6ea75f0fbe9ea3
cornel <ghita.cornel@gmail.com>
2021-03-18 18:53:36 +0200

    bt

--- src/test/java/queries/simple/TestStringProcessing.java
+++ src/test/java/queries/simple/TestStringProcessing.java
@@ -1,159 +1,122 @@
 package queries.simple;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import setup.TransactionalSetup;
 
 public class TestStringProcessing extends TransactionalSetup {
 
-    @Test
-    public void testCONCAT() {
+    SimpleQueryEntity entity1;
+    SimpleQueryEntity entity2;
+    SimpleQueryEntity entity3;
+    SimpleQueryEntity entity4;
+    SimpleQueryEntity entity5;
+    SimpleQueryEntity entity6;
 
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName("name 1");
-        entity.setValue(1);
-        persist(entity);
+    @Before
+    public void addData(){
+
+        entity1 = new SimpleQueryEntity();
+        entity1.setId(1);
+        entity1.setName("name 1");
+        entity1.setValue(1);
+
+        entity2 = new SimpleQueryEntity();
+        entity2.setId(2);
+        entity2.setName(" name ");
+        entity2.setValue(2);
+
+        entity3 = new SimpleQueryEntity();
+        entity3.setId(3);
+        entity3.setName("%name%");
+        entity3.setValue(3);
+
+        entity4 = new SimpleQueryEntity();
+        entity4.setId(4);
+        entity4.setName("Name");
+        entity4.setValue(4);
+
+        entity5 = new SimpleQueryEntity();
+        entity5.setId(5);
+        entity5.setName("Names");
+        entity5.setValue(5);
+
+        entity6 = new SimpleQueryEntity();
+        entity6.setId(6);
+        entity6.setName("abcabcabc");
+        entity6.setValue(6);
+
+        persist(entity1);
+        persist(entity2);
+        persist(entity3);
+        persist(entity4);
+        persist(entity5);
+        persist(entity6);
         flushAndClear();
 
+    }
+
+    @Test
+    public void testCONCAT() {
         Assert.assertEquals("abc name 1", em.createQuery("select concat('abc ',name) from SQE where id = 1", String.class).getSingleResult());
     }
 
     @Test
     public void testTRIM() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName(" name ");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals(" name ", em.createQuery("select name from SQE where id = 1", String.class).getSingleResult());
-        Assert.assertEquals("name", em.createQuery("select trim(name) from SQE where id = 1", String.class).getSingleResult());
+        Assert.assertEquals(" name ", em.createQuery("select name from SQE where id = 2", String.class).getSingleResult());
+        Assert.assertEquals("name", em.createQuery("select trim(name) from SQE where id = 2", String.class).getSingleResult());
     }
 
     @Test
     public void testTRIM_LEADING() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName(" name ");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals(" name ", em.createQuery("select name from SQE where id = 1", String.class).getSingleResult());
-        Assert.assertEquals("name ", em.createQuery("select trim(leading from name) from SQE where id = 1", String.class).getSingleResult());
+        Assert.assertEquals(" name ", em.createQuery("select name from SQE where id = 2", String.class).getSingleResult());
+        Assert.assertEquals("name ", em.createQuery("select trim(leading from name) from SQE where id = 2", String.class).getSingleResult());
     }
 
     @Test
     public void testTRIM_TRAILING() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName(" name ");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals(" name ", em.createQuery("select name from SQE where id = 1", String.class).getSingleResult());
-        Assert.assertEquals(" name", em.createQuery("select trim(trailing from name) from SQE where id = 1", String.class).getSingleResult());
+        Assert.assertEquals(" name ", em.createQuery("select name from SQE where id = 2", String.class).getSingleResult());
+        Assert.assertEquals(" name", em.createQuery("select trim(trailing from name) from SQE where id = 2", String.class).getSingleResult());
     }
 
     @Test// same as simple trim()
     public void testTRIM_BOTH() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName(" name ");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals(" name ", em.createQuery("select name from SQE where id = 1", String.class).getSingleResult());
-        Assert.assertEquals("name", em.createQuery("select trim(both from name) from SQE where id = 1", String.class).getSingleResult());
+        Assert.assertEquals(" name ", em.createQuery("select name from SQE where id = 2", String.class).getSingleResult());
+        Assert.assertEquals("name", em.createQuery("select trim(both from name) from SQE where id = 2", String.class).getSingleResult());
     }
 
     @Test
     public void testTRIM_BOTH_CUSTOM_CHARACTER() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName("%name%");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals("%name%", em.createQuery("select name from SQE where id = 1", String.class).getSingleResult());
-        Assert.assertEquals("name", em.createQuery("select trim(both '%' from name) from SQE where id = 1", String.class).getSingleResult());
+        Assert.assertEquals("%name%", em.createQuery("select name from SQE where id = 3", String.class).getSingleResult());
+        Assert.assertEquals("name", em.createQuery("select trim(both '%' from name) from SQE where id = 3", String.class).getSingleResult());
     }
 
     @Test
     public void testLOWER() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName("Name");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals("Name", em.createQuery("select name from SQE where id = 1", String.class).getSingleResult());
-        Assert.assertEquals("name", em.createQuery("select lower(name) from SQE where id = 1", String.class).getSingleResult());
+        Assert.assertEquals("Name", em.createQuery("select name from SQE where id = 4", String.class).getSingleResult());
+        Assert.assertEquals("name", em.createQuery("select lower(name) from SQE where id = 4", String.class).getSingleResult());
     }
 
     @Test
     public void testUPPER() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName("Name");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals("Name", em.createQuery("select name from SQE where id = 1", String.class).getSingleResult());
-        Assert.assertEquals("NAME", em.createQuery("select upper(name) from SQE where id = 1", String.class).getSingleResult());
+        Assert.assertEquals("Name", em.createQuery("select name from SQE where id = 4", String.class).getSingleResult());
+        Assert.assertEquals("NAME", em.createQuery("select upper(name) from SQE where id = 4", String.class).getSingleResult());
     }
 
     @Test
     public void testSUBSTRING() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName("Names");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals("ame", em.createQuery("select substring(name,2,3) from SQE where id = 1", String.class).getSingleResult());
+        Assert.assertEquals("ame", em.createQuery("select substring(name,2,3) from SQE where id = 5", String.class).getSingleResult());
     }
 
     @Test
     public void testLENGTH() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName("Name");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals(Integer.valueOf(entity.getName().length()), em.createQuery("select length(name) from SQE where id = 1", Integer.class).getSingleResult());
+        Assert.assertEquals(Integer.valueOf(entity4.getName().length()), em.createQuery("select length(name) from SQE where id = 4", Integer.class).getSingleResult());
     }
 
     @Test
     public void testLOCATE() {
-
-        SimpleQueryEntity entity = new SimpleQueryEntity();
-        entity.setId(1);
-        entity.setName("abcabcabc");
-        entity.setValue(1);
-        persist(entity);
-        flushAndClear();
-
-        Assert.assertEquals(Integer.valueOf(4), em.createQuery("select locate('abc',name,2) from SQE where id = 1", Integer.class).getSingleResult());
+        Assert.assertEquals(Integer.valueOf(4), em.createQuery("select locate('abc',name,2) from SQE where id = 6", Integer.class).getSingleResult());
     }
 
 }
